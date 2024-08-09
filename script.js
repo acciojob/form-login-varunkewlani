f2d6f00c0fd8dc50d65dd228cb1a03c2a5d06fc6
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-08-10 00:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function getFormvalue() {
+
+function getFormvalue(event) {
+	event.preventDefault()
+	let a=document.getElementById("fn").value
+	let b=document.getElementById("sn").value
+	let c=a.concat(" ",b)
+
+	alert(c) // or alert(a+" " +b) 
     //Write your code here
 
 }
